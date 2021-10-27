52d2aa548b3647d9b0907f265d898c7ebe4896ef
hi199 <73402456+hi199@users.noreply.github.com>
2021-10-27 20:15:12 +0100

    Add files via upload

---
+++ bank.py
@@ -0,0 +1,18 @@
+class  Bank(object):
+    def __init__(self, pin, cashWithdrawls,balance):
+        self.pin = pin
+        self.cashWithdrawls=cashWithdrawls
+        self.balance=balance
+        
+        def cardPin(self):
+            print("pin")
+            
+        def withdrawls(self):
+            print("cashWithdrawls")
+            
+        def cash(self):
+            print("balance")
+            
+        def changear(self,cashWithdrawls):
+            print("Withdraw cash 100")
+        
